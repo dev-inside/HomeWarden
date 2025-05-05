4f4408f70955231bb1a30a5139619cb3473e677b
Konrad Schneider <konrad.schneider@jobrouter.com>
2025-05-05 16:49:09 +0200

    switch to curl for the selfhst

--- update-icons.sh
+++ update-icons.sh
@@ -1,2 +1,8 @@
-cd ./selfhst-icons
-git pull origin main
+curl -L -o repo.zip https://github.com/selfhst/icons/archive/refs/heads/main.zip
+
+unzip repo.zip -d temp-icons
+
+mkdir -p selfhst-icons
+mv temp-icons/icons-main/* selfhst-icons/
+
+rm -rf temp-icons repo.zip
