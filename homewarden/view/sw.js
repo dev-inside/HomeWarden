cc3a84986e169f5b29c8fb062ec4d821c22e87c4
Konrad Schneider <konrad.schneider@jobrouter.com>
2025-05-09 19:01:02 +0200

    0.6.4: fixed aggressive sw-cache

--- homewarden/view/sw.js
+++ homewarden/view/sw.js
@@ -29,30 +29,41 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          console.log('Serving from cache:', event.request.url);
-          return response;
-        }
-        return fetch(event.request).then((networkResponse) => {
-
-          if (!networkResponse.ok) {
-            throw new Error('Network response was not ok');
+    caches.match(event.request).then((cachedResponse) => {
+      
+      if (cachedResponse) {
+        console.log('Serving from cache:', event.request.url);
+      
+        fetch(event.request).then((networkResponse) => {
+          if (networkResponse && networkResponse.ok) {
+            return caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, networkResponse.clone());
+              console.log('Updated cache with new data:', event.request.url);
+            });
           }
-
-          return caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          });
         }).catch((error) => {
-          console.error('Fetch failed; returning offline page instead.', error);
-
-          return new Response('Offline: Unable to fetch resource', {
-            status: 404,
-            statusText: 'Not Found',
-          });
+          console.error('Network fetch failed:', error);
         });
-      })
+
+        return cachedResponse; 
+      }
+
+      return fetch(event.request).then((networkResponse) => {
+        if (!networkResponse.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      }).catch((error) => {
+        console.error('Fetch failed; returning offline page instead.', error);
+        return new Response('Offline: Unable to fetch resource', {
+          status: 404,
+          statusText: 'Not Found',
+        });
+      });
+    })
   );
 });
